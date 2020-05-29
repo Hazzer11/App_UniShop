4fa9d1fadb7f7212cbd8ec5f99ee4630cb0320ba
Harry <lodgeharry@gmail.com>
2020-05-29 20:32:35 +1000

    Added Item class and image assets for temp item database

--- Assignment_2/app/src/main/java/com/example/assignment_2/Main2Activity.java
+++ Assignment_2/app/src/main/java/com/example/assignment_2/Main2Activity.java
@@ -7,16 +7,42 @@ import android.app.Fragment;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.List;
+
 public class Main2Activity extends AppCompatActivity {
+    List<Item> itemDatabase;
+    Integer[] imageList = { R.drawable.cola, R.drawable.milk,R.drawable.chicken_drumstick, R.drawable.orange_drink, R.drawable.strawberry_milkshake};
+    String[] nameList = {"Cola", "Milk", "Chicken", "Fanta", "Strawberry Milk"};
+    String[] descriptionList={"Brown sweet drink","liquid cow","Delicious chicken","orange drink that i hate","Pink liquid cow"};
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        swapToHomePage();
+
+
+        for (int i= 0; i< imageList.length; i++ )
+        { int image = imageList[i];
+            String name = nameList[i];
+            String description = descriptionList[i];
+
+            Item item = new Item (i, image, name, description);
+            itemDatabase.add(item);
+
+        }
+    }
+
+    public void swapToHomePage(){
         Fragment tempFrag= new HomePage();
 
         FragmentManager fm= getFragmentManager();
@@ -26,9 +52,7 @@ public class Main2Activity extends AppCompatActivity {
         ft.commit();
     }
 
-
-
-    public void oneClicked(View view){
+    public void swapToShoppingList(View view){
         Fragment tempFrag= new ShoppingList();
 
         FragmentManager fm= getFragmentManager();
@@ -38,7 +62,7 @@ public class Main2Activity extends AppCompatActivity {
         ft.commit();
     }
 
-    public void twoClicked(View view){
+    public void swapToItemSearch(View view){
         Fragment tempFrag= new ItemSearch();
 
         FragmentManager fm= getFragmentManager();
@@ -48,8 +72,88 @@ public class Main2Activity extends AppCompatActivity {
         ft.commit();
     }
 
-    public void threeClicked(View view){
+    public void swapToCouponList(View view){
         Fragment tempFrag= new CouponList();
+
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.fragmentMain,tempFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void swapToPolicies(View view){
+        Fragment tempFrag= new Policies();
+
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.fragmentMain,tempFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void swapToSettingsPage(View view){
+        Fragment tempFrag= new SettingsPage();
+
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.fragmentMain,tempFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void swapToShoppingListView(View view){
+        Fragment tempFrag= new ShoppingListView();
+
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.fragmentMain,tempFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void swapToCouponInfo(View view){
+        Fragment tempFrag= new CouponInfo();
+
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.fragmentMain,tempFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void swapToFeedbackPage(View view){
+        Fragment tempFrag= new FeedbackPage();
+
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.fragmentMain,tempFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void swapToItemDisplay(View view){
+        Fragment tempFrag= new ItemDisplay();
+
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.fragmentMain,tempFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void swapToLocationInformationPage(View view){
+        Fragment tempFrag= new LocationInformationPage();
+
+        FragmentManager fm= getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.fragmentMain,tempFrag);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    public void swapToLocationSelectionPage(View view){
+        Fragment tempFrag= new LocationSelectionPage();
 
         FragmentManager fm= getFragmentManager();
         FragmentTransaction ft =fm.beginTransaction();
